422b28671c7f8a995e22da014e233d17e2577996
Saurav Agarwal <agr.saurav1@gmail.com>
2025-01-15 20:36:56 -0500

    fix test_requires error

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
     maintainer_email='sauravag@upenn.edu',
     description='TODO: Package description',
     license='TODO: License declaration',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'idf = cc_rviz.idf:main'
